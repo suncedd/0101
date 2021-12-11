4f98c99f56f24b420850676d03ddc6a62832aefa
suncedd <73060347+suncedd@users.noreply.github.com>
2021-12-11 13:17:29 +0500

    Add files via upload

---
+++ project/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(62, 152);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(286, 152);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(111, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(495, 152);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(191, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X^2+";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(423, 144);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "X+";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(263, 216);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(164, 95);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "кнопочка)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(495, 216);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 95);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "перегрузка методов Разность";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(31, 216);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 95);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "кнопочка)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ project/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Convert.ToDouble(textBox1.Text);
+            double b = Convert.ToDouble(textBox2.Text);
+            double c = Convert.ToDouble(textBox3.Text);
+            Uravnenie ur = new Uravnenie(a, b, c);
+            ur.calculation();
+            ur++;
+            ur.pok();
+            ur--;
+            ur.pok();
+        }
+        class Uravnenie
+        {
+            public double a, b, c, x1, x2,d;
+
+            public Uravnenie(double a, double b, double c)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
+            public void calculation()
+            {
+               d = b * b - 4 * a * c;
+               x1 = (-b - Math.Sqrt(d)) / (2 * a);
+               x2 = (-b + Math.Sqrt(d)) / (2 * a);
+                MessageBox.Show(a+ " " + b + " " + c + " " +"\n" + "Дискриминант= " + Convert.ToString(d) + "\n" + "x1= " + Convert.ToString(x1) + "\n" + "x2 = " + Convert.ToString(x2));
+            }
+            public static Uravnenie operator ++(Uravnenie ur)
+            {
+                ur.a++;
+                ur.b++;
+                ur.c++;
+                return ur;
+            }
+            public static Uravnenie operator --(Uravnenie ur)
+            {
+                ur.a--;
+                ur.b--;
+                ur.c--;
+                return ur;
+            }
+            public void pok()
+            {
+                MessageBox.Show(a + " " + b + " " + c);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = Convert.ToInt32(textBox3.Text);
+            raz(a, b);
+            raz(a, b, c);
+        }
+        public void raz(int a, int b)
+        {
+            int r = a - b;
+            MessageBox.Show("Разность первых 2-х чисел = " + r);
+        }
+        public void raz(int a, int b, int c)
+        {
+            int r = a - b - c;
+            MessageBox.Show("Разность всех 3-х чисел = " + r);
+        }
+       
+        
+        
+        
+        
+        
+        class UravnenieArray
+        {
+           public Uravnenie[] arr;
+            public int size;
+            public UravnenieArray()
+            {
+                arr = null;
+                size = 0;
+            }
+            public UravnenieArray(int s)
+            {
+                size = s;
+                arr = new Uravnenie[size];
+                Random rnd = new Random();
+                for (int i = 0; i < size-1; i++)
+                {
+                    int a = rnd.Next(-10, 10);
+                    int b = rnd.Next(0, 10);
+                    int c = rnd.Next(0, 10);
+                    Uravnenie f = new Uravnenie(a, b, c);
+                    arr[i] = f;
+                }
+            }
+            public void Show()
+            {
+                for (int i = 0; i < size-1; i++)
+                {
+                    arr[i].pok();
+                }
+
+            }
+            public Uravnenie this [int index]
+            {
+                get
+                {
+                    if (index >= 0 && index < size)
+                        return arr[index];
+                    else
+                    {
+                        MessageBox.Show("неправильно задан индекс");
+                        return new Uravnenie(0, 0, 0);
+                    }
+                }
+                set
+                {
+                    if (index >= 0 && index < size)
+                        arr[index] = value;
+                    else
+
+                        MessageBox.Show("неправильно задан индекс");
+                }
+            }
+            public void UR()
+            {
+                for (int i = 0; i < size-1; i++)
+                {
+                    arr[i].calculation();
+
+                }
+                double max = arr[0].x1;
+                double max1 = arr[0].x2;
+                for (int i = 0; i < size-1; i++)
+                {
+                    if (double.IsInfinity(max))
+                    {
+                         max = arr[i].x1;
+                    }
+                    if (double.IsNaN(max)) 
+                    {
+                        max = arr[i].x1;
+                    }
+                    if(max > arr[i].x1)
+                    {
+                        max = arr[i].x1;
+                    }
+                    if (double.IsInfinity(max1))
+                    {
+                        max1 = arr[i].x2;
+                    }
+                    if (double.IsNaN(max1))
+                    {
+                        max1 = arr[i].x2;
+                    }
+                    if (max1 > arr[i].x2)
+                    {
+                        max1 = arr[i].x2;
+                    }
+                }
+                if (max > max1)
+                {
+                    MessageBox.Show("Максимальное абсолютное значение корня = " + max);
+                }
+                else
+                {
+                    MessageBox.Show("Максимальное абсолютное значение корня = " + max1);
+                }
+                }
+
+            }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            UravnenieArray Ua = new UravnenieArray(3);
+            Uravnenie U = new Uravnenie(0,0,0);
+            U = Ua[0];     
+            Ua.Show();
+            Ua.UR();
+
+        }
+    }
+}
